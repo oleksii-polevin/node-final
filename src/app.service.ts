2902239ea3e27f3d0e7b8a27b634f4bf4c7a0341
Oleksii Polevin <pololex327@gmail.com>
2020-04-22 19:09:00 +0300

    style: add some links

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, World!';
+    return `<a href="https://agile-reef-20360.herokuapp.com/users">users</a><br>
+    <a href="https://agile-reef-20360.herokuapp.com/books">books</a>`;
   }
 }
